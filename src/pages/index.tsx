4a9c29d3f24bbedcbe4b657e650b802915731153
thiagoklemga <thiagoklemg@outlook.com>
2023-03-23 18:51:39 -0300

    Remove unused console

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,7 +13,6 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-        {/* {!user.is  <SignInButton />} */}
         {!user.isSignedIn && <SignInButton />}
         {!!user.isSignedIn && <SignOutButton />}
       </main>
