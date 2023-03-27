9bbdb57c7c9f61295fa39f37961d709f08cefac2
thiagoklemga <thiagoklemg@outlook.com>
2023-03-27 20:01:04 -0300

    Profile api added and integrated to pages

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,18 +1,9 @@
 import { privateProcedure } from "./../trpc";
-import { type User } from "@clerk/nextjs/dist/api";
 import { clerkClient } from "@clerk/nextjs/server";
 import { TRPCError } from "@trpc/server";
-
+import { filterUserForClient } from "~/server/helpers/filterUserForClient";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 import { z } from "zod";
-
-const filterUserForClient = (user: User) => {
-  return {
-    id: user.id,
-    username: user.username,
-    profileImageUrl: user.profileImageUrl,
-  };
-};
 
 import { Ratelimit } from "@upstash/ratelimit";
 import { Redis } from "@upstash/redis";
